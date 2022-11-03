48538389de665527c5712f536a3be352dd715c1a
TawWan <aptakqmf12@gmail.com>
2022-11-03 20:30:00 +0900

    fix

--- components/common/Nav/index.tsx
+++ components/common/Nav/index.tsx
@@ -41,37 +41,10 @@ const Nav = () => {
                 <h2 className="title">SNS Login</h2>
               </div>
               <ul>
-                <GoogleLogin clientId="316528616128-l2tjfkmsjj3enepr1r5jaog8ab960oke.apps.googleusercontent.com" buttonText="구글로 계속하기" onSuccess={responseGoogle} onFailure={responseGoogle} />
                 <li className="naver">네이버 로그인</li>
                 <li className="kakao">카카오 로그인</li>
                 <li className="facebook">페이스북 로그인</li>
-                <li className="google">구글 로그인</li>
-                <li className="naver">
-                  <div className="icon_box">
-                    <Image src="../images/ic_naver_login.png" alt="네이버 아이콘"></Image>
-                  </div>
-                  네이버 로그인
-                </li>
-                <li className="kakao">
-                  <div className="icon_box">
-                    <Image src="../images/ic_kakao_login.png" alt="카카오 아이콘">
-                      {" "}
-                    </Image>
-                  </div>
-                  카카오 로그인
-                </li>
-                <li className="facebook">
-                  <div className="icon_box">
-                    <Image src="../images/ic_facebook_login.png" alt="페이스북 아이콘"></Image>
-                  </div>
-                  페이스북 로그인
-                </li>
-                <li className="google">
-                  <div className="icon_box">
-                    <Image src="../images/ic_google_login.png" alt="구글 아이콘"></Image>
-                  </div>
-                  구글 로그인
-                </li>
+                <GoogleLogin clientId="316528616128-l2tjfkmsjj3enepr1r5jaog8ab960oke.apps.googleusercontent.com" buttonText="구글로 계속하기" onSuccess={responseGoogle} onFailure={responseGoogle} />
               </ul>
             </S.ModalContents>
           </Modal>
