9356274746fff6b405b0d1f7fe96ae59f1b8fea7
TawWan <aptakqmf12@gmail.com>
2022-11-10 20:46:11 +0900

    카카오 로그인 리다이렉트 to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { Title, Introduce, NavBtns, Community } from "@/components/pages/home";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const KAKAKO_CODE = router.asPath.split("code=")[1];
+    KAKAKO_CODE &&
+      fetch(`https://kauth.kakao.com/oauth/token`, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&code=${KAKAKO_CODE}&scope=account_email`,
+      });
+  }, []);
+
   return (
     <div>
       <main>
